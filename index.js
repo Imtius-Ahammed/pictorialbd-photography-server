065e0c819b9e290efbd772764119a4b31683fde1
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-08 15:50:55 +0600

    create more private routes

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ruqflxh.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -27,6 +24,7 @@ async function run(){
       const services = await cursor.skip(1).limit(3).toArray();
       res.send(services)
     })
+ 
 
   }
   finally{
